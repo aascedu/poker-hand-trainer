21b1b993bfb640d5cd0377c523d3e759b297dea6
Arthur Ascedu <arthurascedu@proton.me>
2025-10-29 13:36:11 +0100

    added models Hand and Player, begin of draw_hands() method

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import itertools
 import random
 
@@ -12,22 +14,39 @@ class Deck():
             for card in self.CARDS:
                 self.deck.append(card + color)
         random.shuffle(self.deck)
+        random.shuffle(self.deck)
+        random.shuffle(self.deck)
+        random.shuffle(self.deck)
 
 class Hand():
-    cards: tuple
+    cards: tuple()
+
+    def __init__(self, first_card: str, second_card: str):
+        self.cards = tuple(first_card, second_card)
+
+class Player():
+    hand: Hand
+    position: int
+
+    def __init__(self, max_pos: int):
+        self.position = random.randint(0, max_pos - 1)
 
 class Table():
     num_players: int
     deck: Deck
-    hands: list[Hand]
+    player: Player
 
     def __init__(self, num_players: int):
         self.num_players = num_players
         self.deck = Deck()
+        self.player = Player(self.num_players)
 
-    def draw_cards(self):
-        pass
+    def draw_hands(self):
+        print(self.player.position)
+        for i in range(self.num_players * 2):
+            if i % self.num_players == self.player.position:
+                print(f"Player {self.player.position} got {self.deck.deck[i]}")
 
 
-deck = Deck()
-print(deck.deck)
+table = Table(3)
+table.draw_hands()
